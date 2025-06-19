3ee828d48aacd2458116eaa6c00b64eec7c379bd
Primrose <primrose.njoki@student.moringaschool.com>
2025-06-19 16:01:49 +0300

    crudlab

--- server/testing/app_test.py
+++ server/testing/app_test.py
@@ -1,59 +1,62 @@
 import json
-
 from app import app
 from models import db, Plant
 
 class TestPlant:
     '''Flask application in app.py'''
 
+    def setup_method(self):
+        
+        with app.app_context():
+            db.session.query(Plant).delete()  
+            plant = Plant(
+                name="Test Aloe",
+                image="https://example.com/test-aloe.jpg",
+                price=10.99,
+                is_in_stock=True
+            )
+            db.session.add(plant)
+            db.session.commit()
+
     def test_plant_by_id_get_route(self):
         '''has a resource available at "/plants/<int:id>".'''
         response = app.test_client().get('/plants/1')
-        assert(response.status_code == 200)
+        assert response.status_code == 200
 
     def test_plant_by_id_get_route_returns_one_plant(self):
         '''returns JSON representing one Plant object at "/plants/<int:id>".'''
         response = app.test_client().get('/plants/1')
         data = json.loads(response.data.decode())
 
-        assert(type(data) == dict)
-        assert(data["id"])
-        assert(data["name"])
+        assert isinstance(data, dict)
+        assert "id" in data
+        assert "name" in data
 
     def test_plant_by_id_patch_route_updates_is_in_stock(self):
         '''returns JSON representing updated Plant object with "is_in_stock" = False at "/plants/<int:id>".'''
-        with app.app_context():
-            plant_1 = Plant.query.filter_by(id=1).first()
-            plant_1.is_in_stock = True
-            db.session.add(plant_1)
-            db.session.commit()
-            
         response = app.test_client().patch(
             '/plants/1',
-            json = {
-                "is_in_stock": False,
-            }
+            json={"is_in_stock": False}
         )
         data = json.loads(response.data.decode())
 
-        assert(type(data) == dict)
-        assert(data["id"])
-        assert(data["is_in_stock"] == False)
+        assert isinstance(data, dict)
+        assert data["id"] == 1
+        assert data["is_in_stock"] == False
 
     def test_plant_by_id_delete_route_deletes_plant(self):
-        '''returns JSON representing updated Plant object at "/plants/<int:id>".'''
+        '''returns no content after deleting a plant'''
         with app.app_context():
-            lo = Plant(
+            plant = Plant(
                 name="Live Oak",
-                image="https://www.nwf.org/-/media/NEW-WEBSITE/Shared-Folder/Wildlife/Plants-and-Fungi/plant_southern-live-oak_600x300.ashx",
+                image="https://example.com/live-oak.jpg",
                 price=250.00,
                 is_in_stock=False,
             )
-
-            db.session.add(lo)
+            db.session.add(plant)
             db.session.commit()
-            
-            response = app.test_client().delete(f'/plants/{lo.id}')
-            data = response.data.decode()
+            plant_id = plant.id
 
-            assert(not data)
+        response = app.test_client().delete(f'/plants/{plant_id}')
+        assert response.status_code == 204
+        assert response.data.decode() == ''
